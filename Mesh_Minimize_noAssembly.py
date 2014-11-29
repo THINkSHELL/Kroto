c83689c9132e7f73c43bffc2871201dc7a2023a6
pilcru <pierre.cuvilliers@gmail.com>
2014-11-29 12:02:05 +0100

    Added descent speed coefficient, debug info, face flip based on orientation
    A full speed descent is Newton-Raphson-like and instable. Rhino does not reliably orient mesh faces.
    
    Signed-off-by: pilcru <pierre.cuvilliers@gmail.com>

--- Mesh_Minimize_noAssembly.py
+++ Mesh_Minimize_noAssembly.py
@@ -6,6 +6,7 @@ clr.AddReference("mtrand")
 
 import numpy as np
 import rhinoscriptsyntax as rs
+import Rhino
 
 mesh = rs.GetObject("select mesh", rs.filter.mesh)
 meshi = mesh
@@ -17,6 +18,7 @@ lmax = 1
 connec = rs.MeshFaceVertices(mesh)
 oldVertices = rs.MeshVertices(mesh)
 naked = rs.MeshNakedEdgePoints(mesh)
+vertexFaces = {}
 
 vertices = np.matrix(np.zeros((3, len(oldVertices)))).getT()
 for i, v in enumerate(oldVertices):
@@ -25,25 +27,44 @@ for i, v in enumerate(oldVertices):
     vertices[i, 2] = v[2]
 newVertices = np.matrix(np.copy(vertices))
 var = 2*lmax
-itermax = 10
+itermax = 50
 iter = 0
+speed = 1/2
+
+def upwardFace(x1, x2, x3):
+    v2 = rs.VectorCreate(x2, x1)
+    v3 = rs.VectorCreate(x3, x1)
+    vv = rs.VectorCrossProduct(v2, v3)
+    z = rs.VectorCreate([0,0,1], [0,0,0])
+    a = rs.VectorDotProduct(vv, z)
+    return a > 0
+
+for i, vertex in enumerate(oldVertices):
+    for j, face in enumerate(rs.MeshVertexFaces(mesh, i)):
+        others = list(set(connec[face]) - {i, })
+        [x2, x3] = [oldVertices[n] for n in others]
+        if upwardFace(vertex, x2, x3):
+            vertexFaces[(i, j)] = [i, others[0], others[1]]
+            # print 'not flip'
+        else:
+            vertexFaces[(i, j)] = [i, others[1], others[0]]
+            # print 'flip'
+
 
 while (iter < itermax) & (var > lmax):
     iter += 1
-    print (u"itération {},"
-            + u"\nDéplacement^^2 maximum depuis "
-            + u"l'itération précedente : {} mm²").format(iter, var)
     var = 0
     for i, vertex in enumerate(vertices):
         if not naked[i]:
             qMi = np.matrix(np.zeros([3, 3]))
             qMiX2 = np.matrix(np.zeros([3, 1]))
-
-            for face in rs.MeshVertexFaces(mesh, i):
-                others = list(set(connec[face]) - {i, })
-                [x2, x3] = [vertices[n] for n in others]
+            j = 0
+            # print '####'
+            # print vertex
+            while vertexFaces.get((i, j), 0):
+                x2 = vertices[vertexFaces[(i, j)][1]]
+                x3 = vertices[vertexFaces[(i, j)][2]]
                 x23 = x3 - x2
-                #PB de l'ordre x2/x3 ? Orientation du maillage par Rhino...
                 qMij = q[face] * ( (x23 * x23.getT())[0, 0] 
                                     * np.matrix(np.eye(3))
                                - np.matrix([[x23[0, k]*x23[0, l] 
@@ -52,7 +73,16 @@ while (iter < itermax) & (var > lmax):
                                           ))
                 qMi += qMij
                 qMiX2 += qMij * np.matrix([x2[0, 0], x2[0, 1], x2[0, 2]]).getT()
-            newVertices[i] = (qMi.getI() * qMiX2).getT()
+                # print (i, j)
+                # print x2, x3
+                # print qMij
+                # print qMij * np.matrix([x2[0, 0], x2[0, 1], x2[0, 2]]).getT()
+                # print '---'
+                j += 1
+            # print qMi
+            # print qMiX2
+            newVertices[i] = ( vertices[i]
+                        + speed * ((qMi.getI() * qMiX2).getT()-vertices[i]) )
             if (((newVertices[i] - vertex)*(newVertices[i]-vertex).getT())[0, 0]
                 > var):
                 var = ((newVertices[i] - vertex)*(newVertices[i] - vertex)
@@ -63,9 +93,12 @@ while (iter < itermax) & (var > lmax):
     for i in range(0, len(newVertices)):
         V.append((newVertices[i, 0], newVertices[i, 1], newVertices[i, 2]))
     meshi = rs.AddMesh(V, connec)
+    print (u"itération {},"
+           u"\nDéplacement^2 maximum depuis "
+           u"l'itération précedente : {} mm²").format(iter, var)
     
-rs.HideObject(meshi)
-V = []
-for i in range(0, len(newVertices)):
-    V.append((newVertices[i, 0], newVertices[i, 1], newVertices[i, 2]))
-rs.AddMesh(V, connec)
+# rs.HideObject(meshi)
+# V = []
+# for i in range(0, len(newVertices)):
+#     V.append((newVertices[i, 0], newVertices[i, 1], newVertices[i, 2]))
+# rs.AddMesh(V, connec)
