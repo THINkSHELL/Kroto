106ee619c8eeb648c524906920b94858a017d526
pilcru <pierre.cuvilliers@gmail.com>
2014-12-14 22:34:48 +0100

    Functionalized the whole thing. Added framework for parametric studies.
    Better separation etc.
    
    Signed-off-by: pilcru <pierre.cuvilliers@gmail.com>

--- Mesh_Minimize_noAssembly.py
+++ Mesh_Minimize_noAssembly.py
@@ -8,17 +8,23 @@ import numpy as np
 import rhinoscriptsyntax as rs
 import Rhino
 
+debug = 0
+graphic = 1
+showResult = 0
+
+lmax = 0.001
+
+itermax = 5
+
 mesh = rs.GetObject("select mesh", rs.filter.mesh)
-meshi = mesh
 
 q = np.ones(rs.MeshFaceCount(mesh))
 
-lmax = 1
+if graphic: meshi = mesh
 
 connec = rs.MeshFaceVertices(mesh)
 oldVertices = rs.MeshVertices(mesh)
 naked = rs.MeshNakedEdgePoints(mesh)
-vertexFaces = {}
 
 vertices = np.matrix(np.zeros((3, len(oldVertices)))).getT()
 for i, v in enumerate(oldVertices):
@@ -26,10 +32,6 @@ for i, v in enumerate(oldVertices):
     vertices[i, 1] = v[1]
     vertices[i, 2] = v[2]
 newVertices = np.matrix(np.copy(vertices))
-var = 2*lmax
-itermax = 50
-iter = 0
-speed = 1/2
 
 def upwardFace(x1, x2, x3):
     v2 = rs.VectorCreate(x2, x1)
@@ -39,66 +41,128 @@ def upwardFace(x1, x2, x3):
     a = rs.VectorDotProduct(vv, z)
     return a > 0
 
-for i, vertex in enumerate(oldVertices):
-    for j, face in enumerate(rs.MeshVertexFaces(mesh, i)):
-        others = list(set(connec[face]) - {i, })
-        [x2, x3] = [oldVertices[n] for n in others]
-        if upwardFace(vertex, x2, x3):
-            vertexFaces[(i, j)] = [i, others[0], others[1]]
-            # print 'not flip'
-        else:
-            vertexFaces[(i, j)] = [i, others[1], others[0]]
-            # print 'flip'
+def orientMeshFaces(mesh):
+    vertices = rs.MeshVertices(mesh)
+    connec = rs.MeshFaceVertices(mesh)
+    vertexFaces = {}
+    for i, vertex in enumerate(vertices):
+        for j, face in enumerate(rs.MeshVertexFaces(mesh, i)):
+            others = list(set(connec[face]) - {i, })
+            [x2, x3] = [vertices[n] for n in others]
+            if upwardFace(vertex, x2, x3):
+                vertexFaces[(i, j)] = [i, others[0], others[1]]
+                if debug: print 'not flip'
+            else:
+                vertexFaces[(i, j)] = [i, others[1], others[0]]
+                if debug: print 'flip'
+    return vertexFaces
 
-
-while (iter < itermax) & (var > lmax):
+def iterateVertex(i, vertices, vertexFaces, q, var, speed):
+    qMi = np.matrix(np.zeros([3, 3]))
+    qMiX2 = np.matrix(np.zeros([3, 1]))
+    j = 0
+    if debug: print '####'
+    if debug: print vertex
+    while vertexFaces.get((i, j), 0):
+        x2 = vertices[vertexFaces[(i, j)][1]]
+        x3 = vertices[vertexFaces[(i, j)][2]]
+        x23 = x3 - x2
+        qMij = q[j] * ( (x23 * x23.getT())[0, 0] * np.matrix(np.eye(3))
+                         - np.matrix([[x23[0, k]*x23[0, l] for k in range (0,3)]
+                                                           for l in range(0, 3)]
+                                  )
+                          )
+        qMi += qMij
+        qMiX2 += qMij * np.matrix([x2[0, 0], x2[0, 1], x2[0, 2]]).getT()
+        if debug: print (i, j)
+        if debug: print x2, x3
+        if debug: print qMij
+        if debug: print qMij * np.matrix([x2[0, 0], x2[0, 1], x2[0, 2]]).getT()
+        if debug: print '---'
+        j += 1
+    if debug: print qMi
+    if debug: print qMiX2
+    newVertices[i] = ( vertices[i]
+                + speed * ((qMi.getI() * qMiX2).getT()-vertices[i]) )
+    if (((newVertices[i]-vertices[i])*(newVertices[i]-vertices[i]).getT())[0, 0]
+        > var):
+        var = ((newVertices[i] - vertices[i])*(newVertices[i] - vertices[i])
+               .getT())[0, 0]
+    return var
+    
+def iterateOneStep(vertices, vertexFaces, q, iter, speed):
+    global meshi
     iter += 1
     var = 0
-    for i, vertex in enumerate(vertices):
+    for i in range(len(vertices)):
         if not naked[i]:
-            qMi = np.matrix(np.zeros([3, 3]))
-            qMiX2 = np.matrix(np.zeros([3, 1]))
-            j = 0
-            # print '####'
-            # print vertex
-            while vertexFaces.get((i, j), 0):
-                x2 = vertices[vertexFaces[(i, j)][1]]
-                x3 = vertices[vertexFaces[(i, j)][2]]
-                x23 = x3 - x2
-                qMij = q[face] * ( (x23 * x23.getT())[0, 0] 
-                                    * np.matrix(np.eye(3))
-                               - np.matrix([[x23[0, k]*x23[0, l] 
-                                            for k in range (0,3)]
-                                            for l in range(0, 3)]
-                                          ))
-                qMi += qMij
-                qMiX2 += qMij * np.matrix([x2[0, 0], x2[0, 1], x2[0, 2]]).getT()
-                # print (i, j)
-                # print x2, x3
-                # print qMij
-                # print qMij * np.matrix([x2[0, 0], x2[0, 1], x2[0, 2]]).getT()
-                # print '---'
-                j += 1
-            # print qMi
-            # print qMiX2
-            newVertices[i] = ( vertices[i]
-                        + speed * ((qMi.getI() * qMiX2).getT()-vertices[i]) )
-            if (((newVertices[i] - vertex)*(newVertices[i]-vertex).getT())[0, 0]
-                > var):
-                var = ((newVertices[i] - vertex)*(newVertices[i] - vertex)
-                        .getT())[0, 0]
+            var = iterateVertex(i, vertices, vertexFaces, q, var, speed)
     vertices = np.matrix(np.copy(newVertices))
+    if graphic:
+        rs.HideObject(meshi)
+        V = []
+        for i in range(0, len(newVertices)):
+            V.append((newVertices[i, 0], newVertices[i, 1], newVertices[i, 2]))
+        meshi = rs.AddMesh(V, connec)
+        print (u"itération {},"
+               u"\nDéplacement^2 maximum depuis "
+               u"l'itération précedente : {} mm²").format(iter, var)
+    else:
+        print var
+    return iter, var, vertices
+    
+def meshDistance(vertices, objective):
+    distance = 0
+    for i in range(len(vertices)):
+        if (((objective[i]-vertices[i])*(objective[i]-vertices[i]).getT())[0, 0]
+            > distance):
+            distance = ((objective[i] - vertices[i])*(objective[i] - vertices[i])
+                   .getT())[0, 0]
+    return distance
+    
+def minimizeMesh(vertices, vertexFaces, q, itermax, lmax, reference, speed):
+    iter = 0
+    var = 2*lmax
+    distances = []
+    while (iter < itermax) & (var > lmax):
+        iter, var, vertices = iterateOneStep(vertices, vertexFaces, q, iter, speed)
+        distances.append(meshDistance(vertices, reference))
+    return vertices, distances
+    
+vertexFaces = orientMeshFaces(mesh)
+
+objective, distances = minimizeMesh(vertices, vertexFaces, q, 100, 0.001, vertices, 0.5)
+numspeeds = 10
+distance = []
+
+def convergenceStudy(speed):
+    print '----'
+    print speed
+    distance = minimizeMesh(
+                vertices, vertexFaces, q, 100, 0.001, vertices, speed)[1][::-1]
+    print distance
+    try:
+        np.savetxt(
+            "C:\\Users\\Pierre\\Desktop\\" + speed + ".csv",
+            np.asarray(distance),
+            delimiter = ",")
+    except:
+        pass
+
+"""
+for i in range(numspeeds):
+    speed = (i+1)/numspeeds
+    convergenceStudy(speed)
+"""
+
+# convergenceStudy(0.75)
+# convergenceStudy(0.76)
+
+# minimizeMesh(vertices, vertexFaces, q, itermax, lmax, vertices, 0.5)
+    
+if showResult:
     rs.HideObject(meshi)
     V = []
     for i in range(0, len(newVertices)):
         V.append((newVertices[i, 0], newVertices[i, 1], newVertices[i, 2]))
-    meshi = rs.AddMesh(V, connec)
-    print (u"itération {},"
-           u"\nDéplacement^2 maximum depuis "
-           u"l'itération précedente : {} mm²").format(iter, var)
-    
-# rs.HideObject(meshi)
-# V = []
-# for i in range(0, len(newVertices)):
-#     V.append((newVertices[i, 0], newVertices[i, 1], newVertices[i, 2]))
-# rs.AddMesh(V, connec)
+    rs.AddMesh(V, connec)
