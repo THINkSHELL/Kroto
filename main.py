a57b35d091d110e5df1f4c108e5b328a13af589a
Pierre Cuvilliers <pierre.cuvilliers@gmail.com>
2015-04-20 14:38:45 +0200

    Add license info in files.
    Signed-off-by: Pierre Cuvilliers <pierre.cuvilliers@gmail.com>

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+#  Kroto Membrane Form-finding.
+#  Copyright © 2015, Thinkshell & Laboratoire Navier, ENPC.
+#
+#  This file is part of Kroto.
+#
+#  Kroto is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of
+#  the License, or (at your option) any later version.
+#
+#  Kroto is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Kroto. If not, see http://www.gnu.org/licenses/.
+#
 
 import meshminimize as mm
 import rhinoscriptsyntax as rs
--- vectorworks.py
+++ vectorworks.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+#  Kroto Membrane Form-finding.
+#  Copyright © 2015, Thinkshell & Laboratoire Navier, ENPC.
+#
+#  This file is part of Kroto.
+#
+#  Kroto is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as
+#  published by the Free Software Foundation, either version 3 of
+#  the License, or (at your option) any later version.
+#
+#  Kroto is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with Kroto. If not, see http://www.gnu.org/licenses/.
+#
+
+"""Provides simple vector and matrix calculus functions.
+"""
+
 id = [[1, 0, 0],
       [0, 1, 0],
       [0, 0, 1]]
